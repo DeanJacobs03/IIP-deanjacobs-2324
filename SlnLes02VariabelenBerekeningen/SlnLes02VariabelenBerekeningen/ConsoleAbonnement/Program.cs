46acf0c57de534c81d5bad64eb54e1c99b6e0457
DeanJacobs03 <dean.jacobs@student.odisee.be>
2023-12-05 14:03:05 +0100

    wpfchat aangepast en deel les 2 push

---
+++ SlnLes02VariabelenBerekeningen/SlnLes02VariabelenBerekeningen/ConsoleAbonnement/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAbonnement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Bestelgegevens");
+            Console.WriteLine(@"============== ");
+
+            Console.Write("- Naam klant: ");
+            string naam_klant = Console.ReadLine();
+
+            Console.Write("- aantal toegangsbeurten: ");
+            int beurten = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("- prijs (b.v. 122,5): ");
+            double prijs = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("- badkledij inbegrepen (typ true of false): ");
+            bool kledij = Convert.ToBoolean(Console.ReadLine());
+
+            Console.Write("- geslacht (druk 'm' of 'v'):");
+            char geslacht = Console.ReadKey().KeyChar;
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+            Console.Write("Samenvatting");
+            Console.WriteLine(@"============");
+
+            Console.WriteLine($"- houder: {naam_klant}");
+            Console.WriteLine($"- geslacht: {geslacht}");
+            Console.WriteLine($"- prijs: €{prijs}");
+            Console.WriteLine($"- aantal beurten: {beurten}");
+            Console.WriteLine($"- incl. badkledij: {kledij}");
+            Console.ReadKey();
+        }
+    }
+        }
