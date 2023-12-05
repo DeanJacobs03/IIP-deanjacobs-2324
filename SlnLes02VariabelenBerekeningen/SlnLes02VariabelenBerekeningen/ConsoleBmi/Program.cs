3fe5a70d735ba90caea02dcb3dd8cebff93c4dce
DeanJacobs03 <dean.jacobs@student.odisee.be>
2023-12-05 15:14:54 +0100

    bmi gemaakt

--- SlnLes02VariabelenBerekeningen/SlnLes02VariabelenBerekeningen/ConsoleBmi/Program.cs
+++ SlnLes02VariabelenBerekeningen/SlnLes02VariabelenBerekeningen/ConsoleBmi/Program.cs
@@ -10,6 +10,24 @@ namespace ConsoleBmi
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(@"BMI CALCULATOR
+==============");
+
+            Console.Write("Lengte (in cm): ");
+            double cm = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Gewicht (in kg): ");
+            double gewicht = Convert.ToDouble(Console.ReadLine());
+
+            double lengte = cm / 100;
+
+            double bmi = gewicht / (lengte * lengte);
+            double BmiAfgerond = Math.Round(bmi, 1);
+
+            Console.Write($"Je BMI bedraagt {BmiAfgerond}");
+
+            Console.ReadKey();
+
         }
     }
 }
