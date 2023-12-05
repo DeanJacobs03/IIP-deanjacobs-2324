2c6c9a98da0d0c4783d9af5cf9d613caf38631e8
DeanJacobs03 <dean.jacobs@student.odisee.be>
2023-12-05 16:12:17 +0100

    begin buffet

--- SlnLes02VariabelenBerekeningen/SlnLes02VariabelenBerekeningen/ConsoleBuffet/Program.cs
+++ SlnLes02VariabelenBerekeningen/SlnLes02VariabelenBerekeningen/ConsoleBuffet/Program.cs
@@ -10,6 +10,28 @@ namespace ConsoleBuffet
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            const double SeizoensBuffet = 18.5;
+            const double DessertenBuffets = 7.95;
+            const double KidsMenu = 8.95;
+
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+            Console.WriteLine(@"WELKOM IN HET BUFFETRESTAURANT
+==============================");
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Aantal seizoensbuffets (€18.5)");
+            double XBuffet = Convert.ToDouble(Console.Read());
+
+
+
+            Console.ReadKey();
         }
     }
 }
