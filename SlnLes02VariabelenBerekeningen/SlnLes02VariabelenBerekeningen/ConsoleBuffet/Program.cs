da83b40f901fbaf6bf2e57dd65d24b053ebce083
DeanJacobs03 <dean.jacobs@student.odisee.be>
2023-12-10 21:41:41 +0100

    tot aan wpfbmi gemaakt

--- SlnLes02VariabelenBerekeningen/SlnLes02VariabelenBerekeningen/ConsoleBuffet/Program.cs
+++ SlnLes02VariabelenBerekeningen/SlnLes02VariabelenBerekeningen/ConsoleBuffet/Program.cs
@@ -12,24 +12,81 @@ namespace ConsoleBuffet
         {
             Console.OutputEncoding = Encoding.UTF8;
 
-            const double SeizoensBuffet = 18.5;
-            const double DessertenBuffets = 7.95;
-            const double KidsMenu = 8.95;
-
-
             Console.ForegroundColor = ConsoleColor.DarkYellow;
 
             Console.WriteLine(@"WELKOM IN HET BUFFETRESTAURANT
 ==============================");
 
             Console.WriteLine();
+
+            Console.WriteLine("Alles is à volonté en dranken zijn inbegrepen!");
+            Console.WriteLine("!!Promotie!! Vandaag ronden we het totaalbedrag af naar beneden!");
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+
+            Console.Write("Aantal seizoensbuffets (€18.5): ");
+            int SeizoensBuffet = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Aantal dessertenbuffets (€7.95): ");
+            int DessertenBuffet = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Aantal kids menus (€8.95): ");
+            int KidsMenus = Convert.ToInt32(Console.ReadLine());
+
             Console.WriteLine();
             Console.WriteLine();
 
-            Console.WriteLine("Aantal seizoensbuffets (€18.5)");
-            double XBuffet = Convert.ToDouble(Console.Read());
+            Console.WriteLine($"Fooi (0 indien geen): ");
+            double Fooi = Convert.ToDouble(Console.ReadLine());
+
+            double Totaal = (SeizoensBuffet * 18.50) + (DessertenBuffet * 7.95) + (KidsMenus + 8.95) + Fooi;
+            Console.WriteLine($"Totaal te betalen: €{Totaal}");
 
 
+            int Afgerond = (int)Math.Floor(Totaal);
+            Console.WriteLine($"Totaal te betalen: €{Afgerond}");
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.Write("Cash betaald: ");
+            int betaald = Convert.ToInt32(Console.ReadLine());
+
+            
+
+            int vijftig = (betaald - Afgerond) / 50;
+            int rest1 = (betaald - Afgerond) - vijftig;
+
+            int twintig = rest1 / 20;
+            int rest2 = rest1 - (twintig * 20);
+
+            int tien = rest2 / 10;
+            int rest3 = rest2 - (tien * 10);
+
+            int vijf = rest3 / 5;
+            int rest4 = rest3 - (vijf * 5);
+
+            int twee = rest4 / 2;
+            int rest5 = rest4 - (twee * 2);
+
+            int een = rest5 / 1;
+            int rest6 = rest5 - (een * 1);
+
+
+            Console.WriteLine("U krijgt terug: ");
+
+            Console.WriteLine($"- {vijftig} briefje(s) van 50");
+            Console.WriteLine($"- {twintig} briefje(s) van 20");
+            Console.WriteLine($"- {tien} briefje(s) van 10");
+            Console.WriteLine($"- {vijf} briefje(s) van 5");
+            Console.WriteLine($"- {twee} briefje(s) van 2");
+            Console.WriteLine($"- {een} briefje(s) van 1");
 
             Console.ReadKey();
         }
--- SlnLes02VariabelenBerekeningen/SlnLes02VariabelenBerekeningen/WpfBmi/MainWindow.xaml.cs
+++ SlnLes02VariabelenBerekeningen/SlnLes02VariabelenBerekeningen/WpfBmi/MainWindow.xaml.cs
@@ -24,5 +24,21 @@ namespace WpfBmi
         {
             InitializeComponent();
         }
+
+        
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double gewicht = Convert.ToDouble(TxbGewicht.Text);
+            double lengte_cm = Convert.ToDouble(TxbLengte.Text);
+
+            double lengte = lengte_cm / 100;
+
+            double bmi = gewicht / (lengte * lengte);
+
+            double AfgerondBmi = Math.Round(bmi, 1);
+
+            LblBmi.Content = $"{AfgerondBmi}"; 
+        }
     }
 }
