f16c241c22be335aebd29a71ec4d135cea160518
DeanJacobs03 <dean.jacobs@student.odisee.be>
2024-01-31 16:57:23 +0100

    finale commit project
    de console versie werkt zoals gewenst, het programma zal niet stoppen als er een fout in de getypte tekst zit. Ik heb backspace geïmplementeerd en men zal dus met backspace moeten corrigeren om de het scorebord automatisch te laten weergeven.
    
    Ik weet niet of ik het geluid correct heb gedaan, de files zitten in bin/debug ik kreeg het op geen enkele andere manier klaar.
    
    Wat de wpf versie betreft is het werkt oke, de fouten berekening werkt niet zoals het moet en het geluid werkt helemaal niet ondanks ik het op de zelfde manier heb geprobeerd als in de console.

--- SlnProject/typprogramma/Program.cs
+++ SlnProject/typprogramma/Program.cs
@@ -1,121 +1,151 @@
-﻿using System;
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
-namespace typprogramma
+namespace Typprogramma
 {
+    using System;
+    using System.Diagnostics;
+    using WMPLib;
+
     internal class Program
     {
-        private static string[] englishsentences = { "Arrived compass prepare an on as.", "Man request adapted spirits set pressed. Up to denoting subjects sensible feelings it indulged directly. We dwelling elegance do shutters appetite yourself diverted. Our next drew much you with rank. Tore many held age hold rose than our. She literature sentiments any contrasted. Set aware joy sense young now tears china shy." };
-        private static string[] dutchsentences = { "Sedert jaren vraag ik mij af, waartoe zulke dingen dienen, en ik sta verbaasd over de onbeschaamdheid, waarmee een dichter of romanverteller u iets op de mouw durft spelden, dat nooit gebeurd is, en meestal niet gebeuren kan.", "Dat zijn ook makelaars in koffie, doch hun adres behoeft ge niet te weten. Ik pas er dus wel op, dat ik geen romans schrijf, of andere valse opgaven doe. Ik heb dan ook altijd opgemerkt dat mensen die zich met zoiets inlaten, gewoonlijk slecht wegkomen. Ik ben drieënveertig jaar oud, bezoek sedert twintig jaren de beurs, en kan dus voor de dag treden, als men iemand roept die ondervinding heeft." };
-        private static string[] frenchsentences = { "Le client est très important merci, le client sera suivi par le client. Énée n'a pas de justice, pas de résultat, pas de ligula, et la vallée veut la sauce. Morbi mais qui veut vendre une couche de contenu triste d'internet. Être ivre maintenant, mais ne pas être ivre maintenant, mon urne est d'une grande beauté, mais elle n'est pas aussi bien faite que dans un livre.", "Mécène dans la vallée de l'orc, dans l'élément même. Certaines des exigences faciles du budget, qu'il soit beaucoup de temps pour dignissim et. Je ne m'en fais pas chez moi, ça va être moche dans le vestibule. Mais aussi des protéines de Pour avant la fin de la semaine, qui connaît le poison, le résultat." };
+        private static string[] engelseZin = { "Arrived compass prepare an on as.", "Man request adapted spirits set pressed." };
+        private static string[] nederlandseZin = { "Sedert jaren vraag ik mij af, waartoe zulke dingen dienen, en ik sta verbaasd over de onbeschaamdheid, waarmee een dichter of romanverteller u iets op de mouw durft spelden, dat nooit gebeurd is, en meestal niet gebeuren kan.", "Dat zijn ook makelaars in koffie, doch hun adres behoeft ge niet te weten. Ik pas er dus wel op, dat ik geen romans schrijf, of andere valse opgaven doe. Ik heb dan ook altijd opgemerkt dat mensen die zich met zoiets inlaten, gewoonlijk slecht wegkomen. Ik ben drieënveertig jaar oud, bezoek sedert twintig jaren de beurs, en kan dus voor de dag treden, als men iemand roept die ondervinding heeft." };
+        private static string[] franseZin = { "Le client est très important merci, le client sera suivi par le client. Énée n'a pas de justice, pas de résultat, pas de ligula, et la vallée veut la sauce. Morbi mais qui veut vendre une couche de contenu triste d'internet. Être ivre maintenant, mais ne pas être ivre maintenant, mon urne est d'une grande beauté, mais elle n'est pas aussi bien faite que dans un livre.", "Mécène dans la vallée de l'orc, dans l'élément même. Certaines des exigences faciles du budget, qu'il soit beaucoup de temps pour dignissim et. Je ne m'en fais pas chez moi, ça va être moche dans le vestibule. Mais aussi des protéines de Pour avant la fin de la semaine, qui connaît le poison, le résultat." };
         private static Random rnd = new Random();
-        private static int wrongCharacterCount = 0;
-        private static int totalCharacterCount = 0;
+        private static int foutTekenTeller = 0;
+        private static int totaalTekenTeller = 0;
+        private static TimeSpan timeSpan;
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Console.WriteLine(System.IO.Directory.GetCurrentDirectory());
             Console.WriteLine("Kies een taal:");
             Console.WriteLine("1 Engels");
             Console.WriteLine("2 Nederlands");
             Console.WriteLine("3 Frans");
 
             string userinput = Console.ReadLine();
-
-            switch (userinput) //break niet toegestaan return default wel
-            {
-                case "1":
-                    PrintSentences(englishsentences);
-                    break;
-                case "2":
-                    PrintSentences(dutchsentences);
-                    break;
-                case "3":
-                    PrintSentences(frenchsentences);
-                    break;
-                default:
-                    break;
-            }
-
+            string sentence = GenerateSentence(userinput);
+            PrintSentence(sentence);
             PrintScoreBoard();
 
+            Console.ReadKey();
             Environment.Exit(0);
-
         }
 
-        private static void PrintSentences(string[] sentences)
+        private static string GenerateSentence(string userinput)
         {
-            int index = rnd.Next(0, sentences.Length);
-            string sentence = sentences[index];
+            int index;
+            switch (userinput)
             {
-                string getypteTekst = String.Empty;
-                bool escape = false;
+                case "1":
+                    index = rnd.Next(0, engelseZin.Length);
+                    return engelseZin[index];
+                case "2":
+                    index = rnd.Next(0, nederlandseZin.Length);
+                    return nederlandseZin[index];
+                case "3":
+                    index = rnd.Next(0, franseZin.Length);
+                    return franseZin[index];
+                default:
+                    Console.WriteLine("Ongeldige keuze. Valt standaard terug naar engels.");
+                    index = rnd.Next(0, engelseZin.Length);
+                    return engelseZin[index];
+            }
+        }
 
-                Console.WriteLine(sentence);
-                //gaat door zolang je nog niet alles getypt hebt
-                do
+        private static void PrintSentence(string sentence)
+        {
+            string getypteTekst = string.Empty;
+            bool escape = false;
+            Stopwatch stopwatch = new Stopwatch();
+            WindowsMediaPlayer wmPlayer = new WMPLib.WindowsMediaPlayer();
+            wmPlayer.URL = "sound\\correct.mp3";
+            Console.WriteLine(sentence);
+
+            do
+            {
+                ConsoleKeyInfo ck = Console.ReadKey();
+                if (!stopwatch.IsRunning)
                 {
-                    //typen 
-                    ConsoleKeyInfo ck = Console.ReadKey();
-                    if (ck.Key == ConsoleKey.Escape)
-                    {
-                        escape = true;
-                    }
-                    else if (ck.Key == ConsoleKey.Backspace)
-                    {
-                        getypteTekst = getypteTekst.Remove(getypteTekst.Length - 1);
-                        wrongCharacterCount++;
+                    stopwatch.Start();
+                }
 
-                        UpdateSentence(sentence, getypteTekst);
+                if (ck.Key == ConsoleKey.Escape)
+                {
+                    escape = true;
+                }
+                else if (ck.Key == ConsoleKey.Backspace)
+                {
+                    getypteTekst = getypteTekst.Remove(getypteTekst.Length - 1);
+                    foutTekenTeller++;
+
+                    UpdateZin(sentence, getypteTekst);
+                }
+                else
+                {
+                    totaalTekenTeller++;
+
+                    char letter = ck.KeyChar;
+                    if (letter.Equals(sentence[getypteTekst.Length]))
+                    {
+                        wmPlayer.controls.play();
                     }
                     else
                     {
-                        totalCharacterCount++;
-
-                        char letter = ck.KeyChar;
-                        getypteTekst += letter;
-
-                        UpdateSentence(sentence, getypteTekst);
+                        foutTekenTeller++;
+                        wmPlayer.URL = "sound\\wrong.mp3";
+                        wmPlayer.controls.play();
                     }
+
+                    getypteTekst += letter;
+
+                    UpdateZin(sentence, getypteTekst);
                 }
-                while (!sentence.Equals(getypteTekst) && !escape);
-
-                Console.WriteLine();
-                Console.WriteLine();
-
-                Console.WriteLine("ggestopt met typen"); //bug "g" fix
-
-                Console.ReadKey();
             }
+            while (!sentence.Equals(getypteTekst) && !escape);
+            stopwatch.Stop();
+
+            timeSpan = stopwatch.Elapsed;
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("gestopt met typen");
         }
 
-        private static void UpdateSentence(String sentence, String getypteTekst)
+        private static void UpdateZin(string sentence, string getypteTekst)
         {
             Console.Clear();
-            // dit gaat tekst opnieuw tonen met kleur
             for (int i = 0; i < sentence.Length; i++)
             {
                 if (getypteTekst.Length < i + 1)
                 {
                     Console.ForegroundColor = ConsoleColor.White;
-
                 }
                 else if (sentence[i] == getypteTekst[i])
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
-
                 }
                 else
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                 }
+
                 Console.Write(sentence[i]);
             }
         }
 
         private static void PrintScoreBoard()
         {
-            Console.WriteLine($"Het aantal fouten bedraagt: {wrongCharacterCount}.");
-            Console.WriteLine($"Fout percentage: {(float)wrongCharacterCount / (float)totalCharacterCount}.");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"Het aantal fouten bedraagt: {foutTekenTeller}.");
+            Console.WriteLine($"Fout percentage: {Math.Round((float)foutTekenTeller / (float)totaalTekenTeller * 100, 2)}%.");
+            Console.WriteLine($"Totale tijd: {Math.Round(timeSpan.TotalSeconds, 2)}s.");
+            Console.WriteLine($"Gemiddelde tijd per toetsindruk: {Math.Round(timeSpan.TotalSeconds / totaalTekenTeller, 2)}s.");
         }
     }
-
 }
---
+++ SlnProject/wpftypprogramma/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace WpfTypProgramma
+{
+    public partial class MainWindow : Window
+    {
+        private static string[] engelsezin = { "Arrived compass prepare an on as.", "Man request adapted spirits set pressed." };
+        private static string[] nederlandsezin = { "Sedert jaren vraag ik mij af, waartoe zulke dingen dienen, en ik sta verbaasd over de onbeschaamdheid, waarmee een dichter of romanverteller u iets op de mouw durft spelden, dat nooit gebeurd is, en meestal niet gebeuren kan.", "Dat zijn ook makelaars in koffie, doch hun adres behoeft ge niet te weten. Ik pas er dus wel op, dat ik geen romans schrijf, of andere valse opgaven doe. Ik heb dan ook altijd opgemerkt dat mensen die zich met zoiets inlaten, gewoonlijk slecht wegkomen. Ik ben drieënveertig jaar oud, bezoek sedert twintig jaren de beurs, en kan dus voor de dag treden, als men iemand roept die ondervinding heeft." };
+        private static string[] fransezin = { "Le client est très important merci, le client sera suivi par le client. Énée n'a pas de justice, pas de résultat, pas de ligula, et la vallée veut la sauce. Morbi mais qui veut vendre une couche de contenu triste d'internet. Être ivre maintenant, mais ne pas être ivre maintenant, mon urne est d'une grande beauté, mais elle n'est pas aussi bien faite que dans un livre.", "Mécène dans la vallée de l'orc, dans l'élément même. Certaines des exigences faciles du budget, qu'il soit beaucoup de temps pour dignissim et. Je ne m'en fais pas chez moi, ça va être moche dans le vestibule. Mais aussi des protéines de Pour avant la fin de la semaine, qui connaît le poison, le résultat." };
+        private static Random rnd = new Random();
+        private static int foutTekenTeller = 0;
+        private static int totaalTekenTeller = 0;
+        private static Stopwatch stopwatch;
+        private static MediaPlayer correctGeluid;
+        private static MediaPlayer verkeerdGeluid;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            stopwatch = new Stopwatch();
+            correctGeluid = new MediaPlayer();
+            correctGeluid.Open(new Uri("sound//correct.mp3", UriKind.Relative));
+            verkeerdGeluid = new MediaPlayer();
+            verkeerdGeluid.Open(new Uri("sound//wrong.mp3", UriKind.Relative));
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (sender is RadioButton radioButton)
+            {
+                string zin = GenereerZin(radioButton.Name);
+                txbZin.Text = zin;
+            }
+        }
+
+        private string GenereerZin(string taal)
+        {
+            int index;
+            switch (taal)
+            {
+                case "RbEngels":
+                    index = rnd.Next(0, engelsezin.Length);
+                    return engelsezin[index];
+                case "RbNederlands":
+                    index = rnd.Next(0, nederlandsezin.Length);
+                    return nederlandsezin[index];
+                case "RbFrans":
+                    index = rnd.Next(0, fransezin.Length);
+                    return fransezin[index];
+                default:
+                    Console.WriteLine("Ongeldige keuze. Valt standaard terug naar engels.");
+                    index = rnd.Next(0, engelsezin.Length);
+                    return engelsezin[index];
+            }
+        }
+
+        private void TextBoxInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string zin = txbZin.Text;
+            string getypteTekst = txbTypen.Text;
+
+            if (!stopwatch.IsRunning)
+            {
+                stopwatch.Start();
+            }
+
+            totaalTekenTeller++;
+
+            UpdateZin(zin, getypteTekst);
+
+            if (getypteTekst.Equals(zin))
+            {
+                stopwatch.Stop();
+                TimeSpan timeSpan = stopwatch.Elapsed;
+                PrintScoreBord(timeSpan);
+            }
+        }
+
+        private void UpdateZin(string zin, string getypteTekst)
+        {
+            txbZin.Inlines.Clear();
+
+            for (int i = 0; i < zin.Length; i++)
+            {
+                Run run = new Run(zin[i].ToString());
+
+                if (i < getypteTekst.Length)
+                {
+                    if (getypteTekst[i] == zin[i])
+                    {
+                        run.Foreground = Brushes.Green;
+                    }
+                    else
+                    {
+                        foutTekenTeller++;
+                        run.Foreground = Brushes.Red;
+                    }
+                }
+
+                txbZin.Inlines.Add(run);
+            }
+        }
+
+        private void PrintScoreBord(TimeSpan timeSpan)
+        {
+            double foutPercentage = Math.Round((double)foutTekenTeller / (double)totaalTekenTeller * 100, 2);
+            double totaalTijdSeconden = Math.Round(timeSpan.TotalSeconds, 2);
+            double gemiddeldToetsaanslagen = Math.Round(timeSpan.TotalSeconds / totaalTekenTeller, 2);
+
+            string scoreBord = $"Het aantal fouten bedraagt: {foutTekenTeller}.\n" +
+                                $"Fout percentage: {foutPercentage}%.\n" +
+                                $"Totale tijd: {totaalTijdSeconden}s.\n" +
+                                $"Gemiddelde tijd per toetsindruk: {gemiddeldToetsaanslagen}s.";
+
+            labelScore.Content = scoreBord;
+        }
+
+        private void ButtonStop_Click(object sender, RoutedEventArgs e)
+        {
+            stopwatch.Stop();
+            TimeSpan timeSpan = stopwatch.Elapsed;
+            PrintScoreBord(timeSpan);
+        }
+    }
+}
